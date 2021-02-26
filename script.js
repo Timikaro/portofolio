04625ce887a37eb79bd890cf24810c1feb37d9eb
Andi_k <75620540+Timikaro@users.noreply.github.com>
2021-02-26 14:39:31 +0700

    Add files via upload
    upload css,js,html

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementsByClassName('why1')[0],
+    renderer :'svg',
+    loop: 'true',
+    autoplay: 'true',
+    path:'https://assets10.lottiefiles.com/private_files/lf30_fi8yfbmc.json'
+})
